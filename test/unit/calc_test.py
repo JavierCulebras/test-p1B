80eaf4dee7dd662a84261474c7a38b969c287c07
JavierCulebras <javi.culebras99@gmail.com>
2025-01-29 00:20:39 +0100

    Test divide by zero added

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -61,6 +61,8 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
+    def test_divided_by_zero(self):
+        self.assertRaises(TypeError, self.calc.divide, 10, 0)
         
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
